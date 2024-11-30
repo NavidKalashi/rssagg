69555905fe92b098e9ce92f72cd49cb7bb6d2cd8
NavidKalashi <kalashinavid@gmail.com>
2024-11-30 12:53:24 +0330

    add feed follows many to many relationships

--- models.go
+++ models.go
@@ -47,8 +47,26 @@ func databaseFeedToFeed(dbFeed database.Feed) Feed {
 
 func databaseFeedsToFeeds(dbFeeds []database.Feed) []Feed {
 	feeds := []Feed{}
-	for _, dbFeed := range dbFeeds{
+	for _, dbFeed := range dbFeeds {
 		feeds = append(feeds, databaseFeedToFeed(dbFeed))
 	}
 	return feeds
+}
+
+type FeedFollow struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	UserId    uuid.UUID `json:"user_id"`
+	FeedID    uuid.UUID `json:"feed_id"`
+}
+
+func databaseFeedFollowToFeedFollow(dbFeedFollow database.FeedFollow) FeedFollow {
+	return FeedFollow{
+		ID:        dbFeedFollow.ID,
+		CreatedAt: dbFeedFollow.CreatedAt,
+		UpdatedAt: dbFeedFollow.UpdatedAt,
+		UserId:    dbFeedFollow.UserID,
+		FeedID:    dbFeedFollow.FeedID,
+	}
 }
