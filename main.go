7ce7d0a0a41f08e7a6fb0c1b431259210ff8b299
NavidKalashi <kalashinavid@gmail.com>
2024-11-30 17:56:43 +0330

    aggregation worker

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/NavidKalashi/rssagg/internal/database"
 	"github.com/go-chi/chi"
@@ -43,9 +44,12 @@ func main(){
 		log.Fatal("can't connect to database:", err)
 	}
 
+	db := database.New(conn)
 	apiCfg := apiConfig{
-		DB:	database.New(conn),
+		DB:	db,
 	}
+
+	go startScraping(db, 10, time.Minute)
 
 	router := chi.NewRouter()
 
